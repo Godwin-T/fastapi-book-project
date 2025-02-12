2defd46f85f00fd3264d3939be9a47a80b6e78e6
Godwin-T <godwinolagunju0@gmail.com>
2025-02-12 16:52:01 +0100

    remove others

--- test.py
+++ test.py
@@ -1,12 +1,3 @@
-# import requests
-
-# url = "http://52.42.17.90/segment"
-
-# data_dict = {"bounding_box": [[ 1261, 511, 1493, 1032]], "input_video":"https://skoutwatch-videos.s3.eu-north-1.amazonaws.com/1739012950196_2932301-uhd_4096_2160_24fps.mp4"}
-# response = requests.post(url, json=data_dict).json()
-# print(response)
-
-
 import requests
 
 url = "http://34.209.240.233//api/v1/books/40"
